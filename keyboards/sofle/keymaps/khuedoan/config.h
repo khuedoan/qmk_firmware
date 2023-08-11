c5eb423dc0eb3c88a614ad779dcf6b196a953289
Khue Doan <mail@khuedoan.com>
2023-08-11 14:13:42 +0700

    refactor: update layout

--- keyboards/sofle/keymaps/khuedoan/config.h
+++ keyboards/sofle/keymaps/khuedoan/config.h
@@ -7,3 +7,10 @@ for more options.
 
 #define SPLIT_USB_DETECT
 #define MASTER_LEFT
+
+// TODO optimize this
+#undef TAPPING_TERM
+#define TAPPING_TERM 100
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_PER_KEY
