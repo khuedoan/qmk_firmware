6f6c8b3e3f7c50eeb7c466dfbf3387303f136e99
Khue Doan <mail@khuedoan.com>
2023-08-14 09:03:35 +0700

    refactor: adjust mod tap and add more mod tap key maps

--- keyboards/sofle/keymaps/khuedoan/config.h
+++ keyboards/sofle/keymaps/khuedoan/config.h
@@ -10,7 +10,5 @@ for more options.
 
 // TODO optimize this
 #undef TAPPING_TERM
-#define TAPPING_TERM 100
-#define TAPPING_TERM_PER_KEY
-#define PERMISSIVE_HOLD
-#define PERMISSIVE_HOLD_PER_KEY
+#define TAPPING_TERM 500
+#define HOLD_ON_OTHER_KEY_PRESS
