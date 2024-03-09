dcc8a540dc8625cc51fd8c93851da79e1026eb54
Khue Doan <mail@khuedoan.com>
2024-03-09 11:41:45 +0700

    refactor: change keymaps again

--- keyboards/sofle/keymaps/khuedoan/keymap.c
+++ keyboards/sofle/keymaps/khuedoan/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_EQL,
         KC_XCAPE,   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       XXXXXXX,             XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_SHIENT,
-        /*    */    /*    */    MO(_NAV),   KC_LALT,    KC_LGUI,    KC_SPC,     MO(_NAV),            KC_BSPC,    KC_NAVSPC,  MO(_FUNC),  KC_RGUI,    KC_RCTL     /*    */    /*    */
+        /*    */    /*    */    MO(_FUNC),  KC_LALT,    KC_LGUI,    KC_SPC,     MO(_NAV),            KC_BSPC,    KC_NAVSPC,  MO(_FUNC),  KC_RALT,    KC_RCTL     /*    */    /*    */
     ),
 
     [_NAV] = LAYOUT(
         _______,    KC_BASE,    KC_GAME,    _______,    _______,    _______,                                     _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    _______,    KC_BTN3,    KC_MS_U,    KC_WH_U,    _______,                                     _______,    _______,    _______,    _______,    KC_LBRC,    KC_RBRC,
-        _______,    KC_BTN1,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_BTN2,                                     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    KC_BSLS,
-        _______,    _______,    _______,    _______,    KC_WH_D,    _______,    _______,             _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    KC_BTN3,    KC_MS_U,    KC_WH_U,    _______,                                     _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    KC_BTN1,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_BTN2,                                     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,
+        _______,    _______,    _______,    _______,    KC_WH_D,    _______,    _______,             _______,    _______,    _______,    KC_LBRC,    KC_RBRC,    KC_BSLS,    _______,
         /*    */    /*    */    _______,    _______,    _______,    _______,    _______,             _______,    _______,    _______,    _______,    _______     /*    */    /*    */
     ),
 
